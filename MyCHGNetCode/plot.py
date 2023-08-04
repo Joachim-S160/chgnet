d2a15a4e2d7d20550922756db103e7a57f1128ef
joaschee <Joachim.Scheerlinck@UGent.be>
2023-08-04 09:41:27 -0700

    .

--- MyCHGNetCode/plot.py
+++ MyCHGNetCode/plot.py
@@ -3,64 +3,96 @@ import numpy as np
 from ase.io.trajectory import Trajectory
 
 
+import numpy as np
+
 def get_THtr(files: list):
     """
     input = list of trajectory files, 
     output = 3D Matrix: 
     1st dimension is the file, 
     2nd dimension is temperature, enthalpy, time, density 
-    3d dimension is the time step
+    3rd dimension is the time step
     """
 
-    # create a 3D array to store the data
-    THtrfiles = np.zeros((len(files), 4))
-    THtrfiles = np.zeros((len(files), 4))
+    # Create a 3D array to store the data
+    num_files = len(files)
+    num_timesteps = max(len(Trajectory(file)) for file in files)
+    THtrfiles = np.zeros((num_files, 4, num_timesteps))
+
     for index, file in enumerate(files):
         traj = Trajectory(file)[:]
+        num_timesteps_file = len(traj)
         Volume = np.array([atoms.get_volume() for atoms in traj])
         Epot = np.array([atoms.get_potential_energy() for atoms in traj])
         # NPT simulation so pressure is constant
         Enthalpy = Epot + Volume
         print(index, file)
-        THtrfiles[index, 0] = [atoms.get_temperature() for atoms in traj]
-        THtrfiles[index, 1] = Enthalpy.tolist()
-        THtrfiles[index, 2] = np.arrange(len(traj) * 0.2).tolist()
-        THtrfiles[index, 3] = (1 / Volume).tolist()
+
+        # Fill the 3D array with data
+        THtrfiles[index, 0, :num_timesteps_file] = [atoms.get_temperature() for atoms in traj]
+        THtrfiles[index, 1, :num_timesteps_file] = Enthalpy
+        THtrfiles[index, 2, :num_timesteps_file] = np.arange(num_timesteps_file) * 0.2
+        THtrfiles[index, 3, :num_timesteps_file] = 1 / Volume
+
     return THtrfiles
 
+def get_molecule_name(file):
+    input_string = file
 
-# traj = Trajectory("chgnet/MyCHGNetCode/mdNPT2_out_TiBr4.traj")[:]
+    # Get the index of the last occurrence of '/'
+    last_slash_index = input_string.rfind('/')
 
-# stress = np.array([atoms.get_stress() for atoms in traj])
-# pressure = 1/3*(stress[:, 0] + stress[:, 1] + stress[:, 2])
-# Volume = np.array([atoms.get_volume() for atoms in traj])
-# numberofatoms = len(traj[0])
+    # Get the index of the first occurrence of '.'
+    dot_index = input_string.find('.')
 
-# density = np.zeros(len(traj))
-# temperature = np.zeros(len(traj))
-# time = np.zeros(len(traj))
-# epot = np.zeros(len(traj))
-# for index, atoms in enumerate(traj):
+    # Extract the substring between the last slash and the first dot (exclusive)
+    desired_substring = input_string[last_slash_index + 12:dot_index]
 
-#     # Calculate volume
-#     volume = atoms.get_volume()
-#     potential_energy = atoms.get_potential_energy()
+    return desired_substring
 
-#     # Calculate density
-#     epot[index] = potential_energy
-#     density[index] = 1 / volume
-#     temperature[index] = atoms.get_temperature()
-#     time[index] = index * 0.2  # in ps
 
 def time_temperature_plot(files: list):
+    # use different colors for different files
+    colors = ['ro', 'bo', 'go', 'yo', 'mo', 'co', 'ko', 'wo']
     THtr = get_THtr(files)
     for index in range(len(files)):
         time = THtr[index, 2]
         temperature = THtr[index, 0]
-        plt.plot(time, temperature, 'ro', label= index)
+        
+        plt.plot(time, temperature, colors[index], label= get_molecule_name(files[index]))
         plt.xlabel('time (ps)')
         plt.ylabel('temperature (K)')
         print(f"Linear regression for {files[index]} = {linear_regression(time, temperature)}")
+    plt.legend()
+    plt.show()
+    
+def time_density_plot(files: list):
+    # use different colors for different files
+    colors = ['ro', 'bo', 'go', 'yo', 'mo', 'co', 'ko', 'wo']
+    THtr = get_THtr(files)
+    for index in range(len(files)):
+        time = THtr[index, 2]
+        density = THtr[index, 3]
+        
+        plt.plot(time, density, colors[index], label= get_molecule_name(files[index]))
+        plt.xlabel('time (ps)')
+        plt.ylabel('density (A^3)')
+        print(f"Linear regression for {files[index]} = {linear_regression(time, density)}")
+    plt.legend()
+    plt.show()
+    
+def temperature_enthalpy_plot(files: list):
+    # use different colors for different files
+    colors = ['ro', 'bo', 'go', 'yo', 'mo', 'co', 'ko', 'wo']
+    THtr = get_THtr(files)
+    for index in range(len(files)):
+        temperature = THtr[index, 0]
+        enthalpy = THtr[index, 1]
+        
+        plt.plot(temperature, enthalpy, colors[index], label= get_molecule_name(files[index]))
+        plt.xlabel('temperature (K)')
+        plt.ylabel('enthalpy (eV)')
+        print(f"Linear regression for {files[index]} = {linear_regression(temperature, enthalpy)}")
     plt.legend()
     plt.show()
 
@@ -74,52 +106,5 @@ def linear_fit(x, y):
     a, b = np.linalg.lstsq(A, y, rcond=None)[0]
     return a, b
   
-time_temperature_plot(['chgnet/MyCHGNetCode/mdNPT2_out_HfF4.traj','chgnet/MyCHGNetCode/mdNPT2_out_LiCl.traj','chgnet/MyCHGNetCode/mdNPT2_out_TiBr4.traj','chgnet/MyCHGNetCode/mdNPT2_out_WCl6.traj','chgnet/MyCHGNetCode/mdNPT2_out_TiI4.traj'])
-
-
-
-# def time_density_plot():
-#     plt.plot(time, density, 'ro')
-#     plt.xlabel('time (ps)')
-#     # unit of angstrom^3
-#     plt.ylabel('density (A^3)')
-#     # a, _ = linear_fit(time, density)
-#     # plt.legend(['rho = %.2f g/cm^3/ps' % a])
-#     plt.show()
-
-
-# def temperature_density_plot():
-#     plt.plot(temperature, density, 'ro')
-#     plt.xlabel('temperature (K)')
-#     plt.ylabel('density (A^3)')
-#     a, _ = linear_fit(temperature, density)
-#     plt.legend(['rho = %.2f A^3/K' % a])
-#     plt.show()
-
-
-# def temperature_potential_plot():
-#     plt.plot(temperature, epot, 'ro')
-#     plt.xlabel('temperature (K)')
-#     plt.ylabel('potential energy (eV)')
-#     # a, _ = linear_fit(temperature, epot)
-#     # plt.legend(['Epot = %.2f eV/K' % a])
-#     plt.show()
-
-
-# def enthalpy_temperature_plot():
-#     pressure = 1
-#     Enthalpy = epot + pressure * Volume
-#     plt.plot(temperature, Enthalpy, 'ro')
-#     plt.xlabel('temperature (K)')
-#     plt.ylabel('enthalpy (eV)')
-#     plt.show()
-
-# # temperature_potential_plot()
-# # time_density_plot()
-# # enthalpy_temperature_plot()
-# # temperature_density_plot()
-# # time_temperature_plot()
-
-
-
-
+# time_temperature_plot(['chgnet/MyCHGNetCode/mdNPT2_out_HfF4.traj','chgnet/MyCHGNetCode/mdNPT2_out_LiCl.traj','chgnet/MyCHGNetCode/mdNPT2_out_TiBr4.traj','chgnet/MyCHGNetCode/mdNPT2_out_WCl6.traj','chgnet/MyCHGNetCode/mdNPT2_out_TiI4.traj'])
+time_density_plot(['chgnet/MyCHGNetCode/mdNPT2_out_HfF4.traj','chgnet/MyCHGNetCode/mdNPT2_out_LiCl.traj','chgnet/MyCHGNetCode/mdNPT2_out_TiBr4.traj','chgnet/MyCHGNetCode/mdNPT2_out_WCl6.traj','chgnet/MyCHGNetCode/mdNPT2_out_TiI4.traj'])
